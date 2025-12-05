3c03ae59a3fbb1f14198d0f9d71677fd6b0af381
okhay-p <oakkarphyo7@gmail.com>
2025-12-05 17:06:04 +0800

    D03p2 go

--- 2025/D03/soln.go
+++ 2025/D03/soln.go
@@ -29,14 +29,53 @@ func main() {
 		}
 
 
-		sI, sV := getMax(numArr[:len(numArr)-1])
-		numArr = numArr[sI+1:]
-		_, eV := getMax(numArr)
-		bankMax := strconv.Itoa(sV) + strconv.Itoa(eV)
+		// part 1
+		// sI, sV := getMax(numArr[:len(numArr)-1])
+		// numArr = numArr[sI+1:]
+		// _, eV := getMax(numArr)
+		// bankMax := strconv.Itoa(sV) + strconv.Itoa(eV)
 
-		fmt.Println(bankMax)
-		bM, _ := strconv.Atoi(bankMax)
-		res += bM
+		// fmt.Println(bankMax)
+		// bM, _ := strconv.Atoi(bankMax)
+		// res += bM
+
+		// part 2
+		ls := 0
+		// fmt.Println("bank:",bank)
+		bankMax := ""
+		for nd := 11; nd >= 0; nd--{
+			// fmt.Println(numArr)
+			// fmt.Println("remaining length",len(numArr))
+			// fmt.Println("nd",nd)
+			if len(numArr) == nd+1{
+				nd = 0
+				strArr := []string {}
+				for _, n := range numArr {
+					ns := strconv.Itoa(n)
+					strArr = append(strArr, ns)
+				}
+				rn := strings.Join(strArr, "")
+				bankMax += rn
+				continue
+			}
+
+			curArr := numArr[:len(numArr)-nd]
+			// fmt.Println("curArr",curArr)
+			cI, cV := getMax(curArr)
+			ls = cI
+			numArr = numArr[ls+1:]
+			// fmt.Println("digit:", 12-nd)
+			// fmt.Println("cV",cV)
+			// fmt.Println("ls",ls)
+			bankMax+=strconv.Itoa(cV)
+			// fmt.Println()
+		}
+		// fmt.Println("bankMax",bankMax)
+		bm, err := strconv.Atoi(bankMax)
+		if err != nil{
+			fmt.Println(err)
+		}
+		res += bm
 
 	}
 	fmt.Println(res)
